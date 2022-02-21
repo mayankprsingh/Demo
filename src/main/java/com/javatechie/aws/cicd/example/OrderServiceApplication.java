a746973834df0bc4f2001c95738245fc92dc3e7c
Mayank Pratap Singh <48886579+mayankprsingh@users.noreply.github.com>
2022-02-21 11:28:08 +0530

    Update OrderServiceApplication.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -18,9 +18,6 @@ public class OrderServiceApplication {
 
     @Autowired
     private OrderDao orderDao;
-    
-    List<Orders> list = oderDao.getOrders();
-    list.forEach(System.out.println);
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
